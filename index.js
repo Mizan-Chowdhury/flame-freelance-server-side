2c51418847cb383af81b0eeb07b1a10e0b0e3062
mizan-chowdhury <mizan1034@gmail.com>
2023-11-07 22:49:51 +0600

    created api for job details data load

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const cors = require('cors');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
@@ -18,24 +18,42 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
+    const addNewJob = client.db("flameFrelanceDB").collection("addJob");
+    // const cartCollection = client.db("").collection("");
     // await client.connect();
     // await client.db("admin").command({ ping: 1 });
-    app.post('/addJob', async (req,res)=>{
-        const newJob = req.body;
-        
-    })
+    app.post("/addJob", async (req, res) => {
+      // const newJob = req.body;
+      console.log(newJob);
+      const result = await addNewJob.insertOne(newJob);
+      res.send(result);
+    });
 
+    app.get("/allJob/:category", async (req, res) => {
+      const category = req.params.category;
+      console.log(category);
+      const query = { category: category };
+      const result = await addNewJob.find(query).toArray();
+      res.send(result);
+    });
 
+    app.get("/jobDetails/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id : new ObjectId(id) };
+      const result = await addNewJob.findOne(query);
+      res.send(result);
+    });
 
-
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  }
-  catch(err){
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } catch (err) {
     console.log(err.message);
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -43,13 +61,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
 // server work testing
 app.get("/", (req, res) => {
   res.send("server is running on UI");
 });
 app.listen(port, (req, res) => {
-    console.log('server is running on port: ',port);
+  console.log("server is running on port: ", port);
 });
